66f0e7c6aacd04cb30349cc89f2ddf08e1d18855
Maximiliaan van Dijk <max@maxilibrium.com>
2022-04-07 13:56:03 +0100

    fix: removed sharp

--- src/core/processImage.ts
+++ src/core/processImage.ts
@@ -1,21 +1,13 @@
 import prisma from '../lib/prisma';
-import sharp from 'sharp';
 import { unlinkSync } from 'fs';
 import { uploadFile } from '../lib/uploadFile';
 
-const tempDir = `${process.env.PWD}/temp`;
-
 const processImage = async (tokenId: string, imagePath: string) => {
-  const filePath = `${tempDir}/${tokenId}.webp`;
-
-  await sharp(imagePath).webp().toFile(filePath);
-
-  await uploadFile(filePath);
+  await uploadFile(imagePath);
 
   await prisma.queue.update({ where: { tokenId }, data: { imgDone: true } });
 
   try {
-    unlinkSync(filePath);
     unlinkSync(imagePath);
   } catch (e) {
     console.error(e);
