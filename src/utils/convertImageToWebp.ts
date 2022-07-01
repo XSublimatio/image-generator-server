4a64f200d1f2bc6859b30e688347084980f6c680
Maximiliaan van Dijk <max@maxilibrium.com>
2022-07-01 00:50:15 -0500

    feat: lossless

--- src/utils/convertImageToWebp.ts
+++ src/utils/convertImageToWebp.ts
@@ -1,7 +1,7 @@
 import sharp from 'sharp';
 
 function convertImageToWebp(buffer: Buffer) {
-  return sharp(buffer).webp({}).toBuffer();
+  return sharp(buffer).webp({ lossless: true }).toBuffer();
 }
 
 export default convertImageToWebp;
